7b4a3704fb8c42e85d0e682404218a76676db1c8
MuugiiU <classroom2leap@gmail.com>
2023-01-11 17:30:49 +0800

    new

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { SocialIcon } from "react-social-icons";
+import { Typewriter } from "react-simple-typewriter";
+import { motion } from "framer-motion";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <motion.div
+          initial={{ x: -1500 }}
+          animate={{ x: 0 }}
+          transition={{ duration: 5 }}
         >
-          Learn React
-        </a>
+          <SocialIcon network="twitter" />
+          <SocialIcon network="facebook" />
+          <SocialIcon network="instagram" />
+          <SocialIcon network="discord" />
+          <SocialIcon network="youtube" />
+        </motion.div>
+
+        <h1 style={{ color: "pink" }}>
+          <Typewriter words={["Sain uu", "Muugii baina"]} loop={false} />
+        </h1>
       </header>
     </div>
   );
