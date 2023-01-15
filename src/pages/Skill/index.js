0ce0ef8d0e72dfd9eaa16948dbe32902fcf58d2d
MuugiiU <classroom2leap@gmail.com>
2023-01-15 12:55:59 +0800

    new

--- src/pages/Skill/index.js
+++ src/pages/Skill/index.js
@@ -1,13 +1,21 @@
 import React from "react";
 import style from "./style.module.css";
 import { WebpackIcon } from "react-webtech-animated-icons";
+import { ReactIcon } from "react-webtech-animated-icons";
+import { cards } from "../../data/skill";
 const Skill = () => {
   return (
     <div id={style.skill}>
       <WebpackIcon
         firstColor="#1C78C0"
         secondColor="#8ED6FB"
-        size={32}
+        size={100}
+        duration={1}
+      />
+      <ReactIcon
+        firstColor="blue"
+        secondColor="#8ED6FB"
+        size={100}
         duration={1}
       />
     </div>
